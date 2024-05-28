008a56b619792cbf02635950b3382c4d18c4c3e8
jerrypacz <jerryboypacquiao@gmail.com>
2024-05-28 16:55:25 +0800

    updated the body part and the footer dropdown

--- index.js
+++ index.js
@@ -1,25 +1,13 @@
+
 // Image Slider
-
 const slideshow = document.querySelectorAll(".mySlides");
-
 let slideIndex = 1;
 showSlides(slideIndex);
-
 
 // Next/previous controls
 function prevSlide(slideshow) {
     showSlides(slideIndex += slideshow);
 }
-// Thumbnail image controls
-// function currentSlide(slideshow) {
-//   showSlides(slideIndex = slideshow);
-// }
-
-// function nextSlide(slideshow) {
-//     showSlides(slideIndex += slideshow);
-//   }
-  
-
 function showSlides(slideshow) {
     let i;
     if (document.querySelector(".carousel")) {
@@ -48,19 +36,6 @@ setInterval(function() {
     prevSlide(1)
   }, 5000);
 
-
-//   var names = [{ name: "jer", lname: "boy"},
-//               { name: "ric", lname: "cho"},
-//               { name: "jer", lname: "xan"}
-//             ]
-            
-
-//             for(let i=0; i<names.length; i++){
-//                 if(names[i].name=="jer" && names[i].lname == "xan"){
-//                     console.log(i)
-//                 console.log(names[i].name)
-//             }
-//         }
 
 
 
